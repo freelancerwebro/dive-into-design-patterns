dd04d1aacda9929c11bef18b3dc7c459f86fc388
Sorin Marian <marian.sorin@yahoo.com>
2020-05-11 12:22:00 +0300

    add unit test for slackNotification class from Adapter pattern

--- tests/Structural/AdapterTest.php
+++ tests/Structural/AdapterTest.php
@@ -3,10 +3,12 @@ require "vendor/autoload.php";
 
 use PHPUnit\Framework\TestCase;
 use DP\Structural\Adapter\EmailNotification;
+use DP\Structural\Adapter\SlackNotification;
+use DP\Structural\Adapter\SlackApi;
 
 class AdapterTest extends TestCase
 {
-    public function testEmailNotificationOutputWorks()
+    public function testIfEmailNotificationOutputWorks()
     {
         $title = "Website is down";
         $message = "Our website is not responding. Call admins and bring it up!";
@@ -15,6 +17,23 @@ class AdapterTest extends TestCase
         $this->expectOutputString($expected);
 
         $notification = new EmailNotification($email);
+        $notification->send($title, $message);
+    }
+
+    public function testIfSlackNotificationOutputWorks()
+    {
+        $title = "Website is down";
+        $message = "Our website is not responding. Call admins and bring it up!";
+        $email = "admin@test.com";
+        $slackLogin = "test.com";
+        $slackApiKey = "XXXXXXXXX-API-KEY-XXXXXXXXX";
+        $slackChatId = "Admins";
+        $slackMessage = "#" . $title . " #" . strip_tags($message);
+        $expected = "Logged into a slack account $slackLogin\nPosted following message into the '$slackChatId' chat: '$slackMessage'\n";
+        $this->expectOutputString($expected);
+
+        $slackApi = new SlackApi($slackLogin, $slackApiKey);
+        $notification = new SlackNotification($slackApi, $slackChatId);
         $notification->send($title, $message);
     }
 }
